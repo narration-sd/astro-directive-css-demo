5ba534ccc1de9a120b39a3dfeccc49379833b509
nsd <narrationsd@gmail.com>
2022-09-08 23:50:43 -0700

    Initial commit - issue not present

---
+++ src/components/SansHydrate.vue
@@ -0,0 +1,29 @@
+<template>
+  <div class="boxed">
+    <h2 class="marked-up">{{ msg }}</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'SansHydrate',
+  props: {
+    msg: { type: String, default: 'I\'m Sans Hydrate' },
+  },
+  setup (props) {
+    console.log('props: ' + JSON.stringify(props))
+  }
+}
+</script>
+
+<style>
+.boxed {
+  max-width: 200px;
+  margin: 20px auto;
+  padding: 10px 50px;
+  background-color: darkgray;
+}
+.marked-up {
+  color: gold;
+}
+</style>
